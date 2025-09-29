ed3b25bae353c4799d2b40054d834722c5300f10
Devesh Kumar <deveshjha46@gmail.com>
2025-09-29 17:06:10 +0530

    Added the pandas operations

--- Pandas/GroupByObject.py
+++ Pandas/GroupByObject.py
@@ -1,6 +1,7 @@
 #%%
 import numpy as np
 import pandas as pd 
+import matplotlib.pyplot as plt
 
 # here we will learn about the group by object 
 
@@ -133,12 +134,12 @@ genre = movies.groupby('Genre')
 
 # Find ranking of each movie in the group according to IMDB score
 
-# def movie_ranking(group):
-#    group['genre_Rank'] = group['IMDB_Rating'].rank(ascending=False)
-#    return group
+def movie_ranking(group):
+   group['genre_Rank'] = group['IMDB_Rating'].rank(ascending=False)
+   return group
 
 
-# print(genre.apply(movie_ranking))
+print(genre.apply(movie_ranking))
 
 
 # Normalized rating on the basis of group 
@@ -148,6 +149,105 @@ def normalized_rating(group):
       return group
 
 print(genre.apply(normalized_rating))
+
+
+## GroupBy on Multiple columns 
+
+#%%
+duo = movies.groupby(['Star1','Director'])
+duo
+
+duo.size()
+duo.get_group(('Amole Gupte','Aamir Khan'))
+
+
+# Find the combo of actor and director most earn 
+
+
+duo['Gross'].sum().sort_values(ascending=False)
+
+
+# Actor with their best genre perpformance on the basis of average metascore 
+movies.groupby(['Star1','Genre'])['Metascore'].mean().reset_index().sort_values('Metascore',ascending=False)
+
+# Aggregate on multiple colums
+
+# print(duo.agg(['min','max','mean']))
+
+
+### Practice on the Grop By Object Complex Problems 
+
+
+#%%
+ipl = pd.read_csv('dataset/deliveries.csv')
+
+ipl.head(1)
+
+# find the top 10 batsman in terms of runs 
+
+# ipl.groupby('batsman')['batsman_runs'].sum().sort_values(ascending=False)
+
+
+# find the batsman with max no. of sixes 
+
+# sixes = ipl[ipl['batsman_runs'] == 6]
+
+# sixes.groupby('batsman')['batsman'].count().sort_values(ascending=False)
+  
+# find batsman with most of 4's and 6's in last 5 overs 
+
+# ipl[ (ipl['batsman_runs'] ==6 )| (ipl['batsman_runs']== 4)]
+
+
+last_overs = ipl[ipl['over'] > 15]
+
+# temp = last_overs[(last_overs['batsman_runs'] == 6) | (last_overs['batsman_runs'] == 4)]
+# temp
+# temp.groupby('batsman').size().sort_values(ascending=False).head(5)
+
+
+
+# Find Virat kohli's record against all teams 
+
+temp_df =ipl[ipl['batsman'] == 'V Kohli']
+temp_df.groupby('bowling_team')['batsman_runs'].sum().reset_index()
+
+
+# Create a function that can return the highest score of any batsman 
+
+temp_run = ipl[ipl['batsman'] == 'V Kohli']
+
+temp_run.groupby('match_id')['batsman_runs'].sum().sort_values(ascending=False).head(1).values[0]
+
+def high_score(batsman):
+      temp_run = ipl[ipl['batsman'] == batsman]
+      return temp_run.groupby('match_id')['batsman_runs'].sum().sort_values(ascending=False).head(1).values[0]
+      
+      
+high_score("MS Dhoni")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
